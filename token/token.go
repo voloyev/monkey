26a07df03a781b18853b982aba7733333c1e71b5
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2024-01-12 16:15:59 +0200

    Update token

--- token/token.go
+++ token/token.go
@@ -8,18 +8,24 @@ type Token struct {
 }
 
 const (
-	ILLEGAL   = "ILLEGAL"
-	EOF       = "EOF"
-	IDENT     = "IDENT"
-	INT       = "INT"
-	ASSIGN    = "="
-	PLUS      = "+"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identificators
+	IDENT = "IDENT"
+	INT   = "INT"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
-	FUNCTION  = "FUNCTION"
-	LET       = "LEY"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LEY"
 )
